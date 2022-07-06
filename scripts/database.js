eaeee14a10e7a883962efa1c981c94072ced5cbc
Toni Baker <tonijbaker5@gmail.com>
2022-07-06 15:15:22 -0400

    all new stuff

---
+++ scripts/database.js
@@ -0,0 +1,77 @@
+const database = {
+
+    services: [{
+       
+        id: 1,
+        name: "Canoeing",
+        destinationId:
+}, {
+        id: 2,
+        name: "Fishing",
+        destinationId:
+}, {
+        id: 3,
+        name: "Hiking",
+        destinationId: 
+
+}, {
+        id: 4,
+        name: "Parking",
+        destinationIs:
+}, {
+        id: 5,
+        name: "Zip Line",
+        destinationId:
+}, {
+        id: 6,
+        name: "Picnicking",
+        destinationId:
+}, {
+        id: 7,
+        name: "Rock Climbing",
+        destinationId    
+}, {
+        id: 8,
+        name: "Lodging",
+        destinationId:
+}, { 
+        id: 9,
+        name: "Rafting",
+        destinationId:
+}, {
+        id: 10,
+        name: "Information",
+        destinationId:
+}],
+
+destinations: [{
+        id: 1,
+        section: "Northeast",
+        name: "Chamfort River"
+},{
+        id: 2,
+        section: "Northeast",
+        name: "Lost Wolf Hiking Trail"
+}, {
+        id: 3,
+        section: "Northwest",
+        name: "Lodge"
+},{
+        id: 4,
+        section: "Southwest",
+        name: "Gander River"
+},{
+        id: 5,
+        section: "Southern",
+        name: "Campgrounds"
+},{
+        id: 6,
+        section: "Southeast",
+        name: "Pine Bluffs Trail"
+}],
+
+
+
+
+
+}
